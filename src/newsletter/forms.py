698e38f86a011b8de77c50563799a912eb22f3fe
Patrick Georges <patrick@Patricks-MacBook-Pro.local>
2015-11-19 16:59:42 -0500

    learning about forms

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -2,6 +2,10 @@ from django import forms
 
 from .models import SignUp
 
+class ContactForm(forms.Form):
+	full_name = forms.CharField()
+	email = forms.EmailField()
+	message = forms.CharField()
 
 class SignUpForm(forms.ModelForm):
 		class Meta:
@@ -13,8 +17,8 @@ class SignUpForm(forms.ModelForm):
 			email = self.cleaned_data.get('email')
 			email_base, provider = email.split("@")
 			domain, extension = provider.split('.')
-			if not extension == "co":
-					raise forms.ValidationError("pls use a viald email address yo")
+			#if not extension == "com":
+					#raise forms.ValidationError("pls use a valid email address yo")
 			return email
 
 
--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
 		title = 'welcome'
-		if request.user.is_authenticated():
-			title = "Current user is %s" %(request.user)
-
+		form = SignUpForm(request.POST or None)
+		#if request.user.is_authenticated():
+		#	title = "Current user is %s" %(request.user)
+		# if request.method =="POST":
+		# print request.POST
 		context = {
-				"template_title": title,
+			"template_title": title,
+			 "form": form
 		}
+
+		if form.is_valid():
+				instance = form.save(commit=False)
+				instance.save()
+				context = {
+					"template_title":"Whoop there it is!"
+				}
+
+
 		return render(request, "home.html", context)
